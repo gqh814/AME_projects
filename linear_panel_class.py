36b20ab5333ac784aadea6c108e9a94a0c72bf9a
gqh814 <124162599+gqh814@users.noreply.github.com>
2024-10-01 15:41:49 +0200

    FE og FD estimation, samt test af antagelser

---
+++ linear_panel_class.py
@@ -0,0 +1,235 @@
+import numpy as np
+from numpy import linalg as la
+from tabulate import tabulate
+
+def estimate( 
+        y: np.ndarray, 
+        x: np.ndarray, 
+        transform='', 
+        T:int=None,
+        robust:bool=False
+    ) -> list:
+    """Uses the provided estimator (mostly OLS for now, and therefore we do 
+    not need to provide the estimator) to perform a regression of y on x, 
+    and provides all other necessary statistics such as standard errors, 
+    t-values etc.  
+
+    Args:
+        >> y (np.ndarray): Dependent variable (Needs to have shape 2D shape)
+        >> x (np.ndarray): Independent variable (Needs to have shape 2D shape)
+        >> transform (str, optional): Defaults to ''. If the data is 
+        transformed in any way, the following transformations are allowed:
+            '': No transformations
+            'fd': First-difference
+            'be': Between transformation
+            'fe': Within transformation
+            're': Random effects estimation.
+        >>t (int, optional): If panel data, t is the number of time periods in
+        the panel, and is used for estimating the variance. Defaults to None.
+
+    Returns:
+        list: Returns a dictionary with the following variables:
+        'b_hat', 'se', 'sigma2', 't_values', 'R2', 'cov'
+    """
+    
+    b_hat = est_ols(y, x)  # Estimated coefficients
+    u_hat = y - x@b_hat  # Calculated residuals
+    SSR = u_hat.T@u_hat  # Sum of squared residuals
+    SST = (y - np.mean(y)).T@(y - np.mean(y))  # Total sum of squares
+    R2 = 1 - SSR/SST
+    
+    sigma2, cov, se = variance(transform, SSR, x, T, u_hat, robust)
+    t_values = b_hat/se
+    
+    names = ['b_hat', 'se', 'sigma2', 't_values', 'R2', 'cov', 'u_hat', 'rob']
+    results = [b_hat, se, sigma2, t_values, R2, cov, u_hat, robust]
+    return dict(zip(names, results))
+
+    
+def est_ols( y: np.ndarray, x: np.ndarray) -> np.ndarray:
+    """Estimates y on x by ordinary least squares, returns coefficents
+
+    Args:
+        >> y (np.ndarray): Dependent variable (Needs to have shape 2D shape)
+        >> x (np.ndarray): Independent variable (Needs to have shape 2D shape)
+
+    Returns:
+        np.array: Estimated beta coefficients.
+    """
+    return la.inv(x.T@x)@(x.T@y)
+
+def variance(
+        transform: str, 
+        SSR: float, 
+        x: np.ndarray, 
+        T: int, 
+        u_hat: np.ndarray,  # Adding residuals for robust SE calculation
+        robust: bool = False  # Optional argument to switch to robust variance calculation
+    ) -> tuple:
+    """Calculates the covariance and standard errors from the OLS
+    estimation, supporting robust (heteroskedasticity-consistent) variance.
+
+    Args:
+        >> transform (str): Specifies the data transformation:
+            '': No transformations
+            'fd': First-difference
+            'be': Between transformation
+            'fe': Within transformation
+            're': Random effects estimation
+        >> SSR (float): Sum of squared residuals.
+        >> x (np.ndarray): Independent variables from regression.
+        >> u_hat (np.ndarray): Residuals from regression for robust variance.
+        >> T (int): The number of time periods.
+        >> robust (bool): If True, calculates robust standard errors.
+
+    Raises:
+        Exception: If an invalid transformation is provided, an error is returned.
+
+    Returns:
+        tuple: Returns the error variance (mean square error),
+               covariance matrix, and standard errors.
+    """
+    
+    # Store n and k, used for DF adjustments.
+    K = x.shape[1]
+    if transform in ('', 'fd', 'be'):
+        N = x.shape[0]
+    else:
+        N = x.shape[0] / T  # For panel data
+    
+    # Calculate sigma2 (error variance) using different methods based on the transformation
+    if transform in ('', 'fd', 'be'):
+        sigma2 = (np.array(SSR / (N - K)))
+    elif transform.lower() == 'fe':
+        sigma2 = np.array(SSR / (N * (T - 1) - K))
+    elif transform.lower() == 're':
+        sigma2 = np.array(SSR / (T * N - K))
+    else:
+        raise Exception('Invalid transform provided.')
+
+    # Variance-covariance matrix calculation
+    if robust: 
+        # Meat and bread 
+        u_hat_squared = np.diag(u_hat.flatten() ** 2) # flatten to 1-D array, so we create a diag matrix. 
+        meat = x.T @ u_hat_squared @ x
+
+        bread = la.inv(x.T @ x)
+
+        cov = bread @ meat @ bread # Sandwich formula
+
+    else:  
+        cov = sigma2 * la.inv(x.T @ x) # Standard formula
+
+    # Standard errors are the square root of the diagonal of the variance-covariance matrix
+    se = np.sqrt(cov.diagonal()).reshape(-1, 1)
+    
+    return sigma2, cov, se
+
+
+def remove_zero_columns(x, label_x):
+    """
+    The function removes columns from a matrix that are all zeros and returns the updated matrix and
+    corresponding labels.
+    
+    Args:
+      x: The parameter `x` is a numpy array representing a matrix with columns that may contain zeros.
+      label_x: The parameter `label_x` is a list that contains the labels for each column in the input
+    array `x`.
+    
+    Returns:
+      x_nonzero: numpy array of x with columns that are all zeros removed.
+      label_nonzero: list of labels for each column in x_nonzero.
+    """
+    
+    # Find the columns that are not all close to zeros
+    nonzero_cols = ~np.all(np.isclose(x,0), axis=0)
+    
+    # Remove the columns that are all zeros
+    x_nonzero = x[:, nonzero_cols]
+    
+    # Get the labels for the columns that are not all zeros
+    label_nonzero = [label_x[i] for i in range(len(label_x)) if nonzero_cols[i]]
+    return x_nonzero, label_nonzero
+
+def print_table(
+        labels: tuple,
+        results: dict,
+        headers=["", "Beta", "Se", "t-values"],
+        title="Results",
+        _lambda:float=None,
+        **kwargs
+    ) -> None:
+    """Prints a nice looking table, must at least have coefficients, 
+    standard errors and t-values. The number of coefficients must be the
+    same length as the labels.
+
+    Args:
+        >> labels (tuple): Touple with first a label for y, and then a list of 
+        labels for x.
+        >> results (dict): The results from a regression. Needs to be in a 
+        dictionary with at least the following keys:
+            'b_hat', 'se', 't_values', 'R2', 'sigma2'
+        >> headers (list, optional): Column headers. Defaults to 
+        ["", "Beta", "Se", "t-values"].
+        >> title (str, optional): Table title. Defaults to "Results".
+        _lambda (float, optional): Only used with Random effects. 
+        Defaults to None.
+    """
+    
+    # Unpack the labels
+    label_y, label_x = labels
+    
+    # Create table, using the label for x to get a variable's coefficient,
+    # standard error and t_value.
+    table = []
+    for i, name in enumerate(label_x):
+        row = [
+            name, 
+            results.get('b_hat')[i], 
+            results.get('se')[i], 
+            results.get('t_values')[i]
+        ]
+        table.append(row)
+    
+    # Print the table
+    print('---------------------------------------------')
+    print(title)
+    print(f"Dependent variable: {label_y}\n")
+    print(tabulate(table, headers, **kwargs))
+    
+    # Print extra statistics of the model.
+    print(f"R\u00b2 = {results.get('R2').item():.3f}")
+    print(f"\u03C3\u00b2 = {results.get('sigma2').item():.3f}")
+    if _lambda: 
+        print(f'\u03bb = {_lambda.item():.3f}')
+    print(f'Robust standard errors: {results.get("rob")}')
+
+
+def perm(Q_T: np.ndarray, A: np.ndarray) -> np.ndarray:
+    """Takes a transformation matrix and performs the transformation on 
+    the given vector or matrix.
+
+    Args:
+        Q_T (np.ndarray): The transformation matrix. Needs to have the same
+        dimensions as number of years a person is in the sample.
+        
+        A (np.ndarray): The vector or matrix that is to be transformed. Has
+        to be a 2d array.
+
+    Returns:
+        np.array: Returns the transformed vector or matrix.
+    """
+    # We can infer t from the shape of the transformation matrix.
+    M,T = Q_T.shape 
+    N = int(A.shape[0]/T)
+    K = A.shape[1]
+
+    # initialize output 
+    Z = np.empty((M*N, K))
+    
+    for i in range(N): 
+        ii_A = slice(i*T, (i+1)*T)
+        ii_Z = slice(i*M, (i+1)*M)
+        Z[ii_Z, :] = Q_T @ A[ii_A, :]
+
+    return Z
